0784e9ef6a82770df538cd71068ee3948708636e
daehyeong2 <baconbacon1231@gmail.com>
2024-10-15 17:57:32 +0900

    1025 문제 풀이

--- 알고리즘/a.cpp
+++ 알고리즘/a.cpp
@@ -1,20 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a, sum;
+int n, m, t, max_ = -1;
+char c;
+vector<vector<char>> v;
+map<int, int> mp = {};
+
 int main() {
-	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		v[i] = a;
-		sum += a;
-	};
-	sort(v.begin(), v.end());
-	for (int i : v) {
-		cout << i << " ";
-	}
-	cout << "\n";
-	cout << fixed << setprecision(2) << sum / (double)n << "\n";
-	return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cin.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        v.push_back({});
+        for (int j = 0; j < m; j++) {
+            cin >> c;
+            v[i].push_back(c);
+        }
+    }
+    while (true) {
+        if (t * t > 999999999) break;
+        mp[t * t] = 1;
+        t++;
+    }
+    if (n == 1 && m == 1) {
+        if (mp[v[0][0] - 48]) cout << v[0][0] << "\n";
+        else cout << -1 << "\n";
+        return 0;
+    }
+    for (int i = -n + 1; i < n; i++) {
+        for (int j = -m + 1; j < m; j++) {
+            if (i == 0 && j == 0) continue;
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < m; l++) {
+                    string str = "";
+                    int i_ = k;
+                    int j_ = l;
+                    while (true) {
+                        str += v[i_][j_];
+                        int ret = stoi(str);
+                        if (mp[ret]) max_ = max(max_, ret);
+                        i_ += i;
+                        if (i_ >= n || i_ < 0) break;
+                        j_ += j;
+                        if (j_ >= m || j_ < 0) break;
+                    }
+                }
+            }
+        }
+    }
+    cout << max_ << "\n";
 }
