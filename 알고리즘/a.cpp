3c7d80b08c630cf1fb4a7c2693c3c4362e6d23ef
daehyeong2 <baconbacon1231@gmail.com>
2025-02-24 17:37:30 +0900

    주난의 난 (14497)

--- 알고리즘/a.cpp
+++ 알고리즘/a.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int max_n = 500000;
-int a, b, visited[2][max_n + 4], turn = 1;
-bool ok;
+int n, m, x, y, dy[] = { -1, 0, 1, 0 }, dx[] = { 0, 1, 0, -1 };
+pair<int, int> ju, cr;
+int arr[304][304], visited[304][304];
+vector<pair<int, int>> v;
+char tmp;
 
 int main() {
-	cin >> a >> b;
-	if (a == b) { cout << 0 << "\n"; return 0; }
-	queue<int> q;
-	visited[0][a] = 1;
-	q.push(a);
-	while (q.size()) {
-		b += turn;
-		if (b > max_n) break;
-		if(visited[turn % 2][b]) {
-			ok = 1; break;
+	cin >> n >> m;
+	cin >> ju.first >> ju.second >> cr.first >> cr.second;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> tmp;
+			if (tmp == '#' || tmp == '1') arr[i][j] = 1;
+			else arr[i][j] = 0;
 		}
-		int qSize = q.size();
-		for (int _ = 0; _ < qSize; _++) {
-			int n = q.front(); q.pop();
-			for (int i : {n + 1, n - 1, n * 2}) {
-				if (i < 0 || i > max_n || visited[turn % 2][i]) continue;
-				visited[turn % 2][i] = visited[(turn + 1) % 2][n] + 1;
-				if (i == b) {
-					ok = 1;
-					break;
-				}
+	}
+	queue<pair<int, int>> q;
+	q.push({ ju.first - 1, ju.second - 1 });
+	visited[ju.first - 1][ju.second - 1] = 1;
+	while (q.size()) {
+		tie(y, x) = q.front(); q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+			if (arr[ny][nx] == 1) {
+				visited[ny][nx] = visited[y][x] + 1;
+				arr[ny][nx] = 0;
+				v.push_back({ ny, nx });
+			}
+			else {
+				visited[ny][nx] = visited[y][x];
+				q.push({ ny, nx });
+			}
+		}
+		if (q.empty()) {
+			for (pair<int, int> i : v) {
 				q.push(i);
 			}
-			if (ok) break;
+			v = {};
 		}
-		if (ok) break;
-		turn++;
 	}
-	if (ok) cout << turn << "\n";
-	else cout << -1 << "\n";
+	cout << visited[cr.first - 1][cr.second - 1] - 1 << "\n";
 }
