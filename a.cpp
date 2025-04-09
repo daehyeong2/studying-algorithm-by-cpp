b4ca7414f4dc8cba849cb4259b63eb1cfcb63d02
daehyeong2 <baconbacon1231@gmail.com>
2025-04-09 09:57:54 +0900

    (BOJ) 1094번 문제

--- a.cpp
+++ a.cpp
@@ -1,50 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-int n, standard[4], d[15][5], price = INF;
-map<int, vector<vector<int> > > ret;
+int n, ret;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for (int & i : standard) cin >> i;
-    for (int i = 0; i < n; i++) for (int & j : d[i]) cin >> j;
-    for (int i = 0; i < (1<<n); i++) {
-        int total[5] = {};
-        for (int j = 0; j < n; j++) {
-            if (i & (1<<j)) {
-                for (int k = 0; k < 5; k++) {
-                    total[k] += d[j][k];
-                }
-             }
-        }
-        bool flag = 0;
-        for (int j = 0; j < 4; j++) {
-            if (total[j] < standard[j]) {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag) continue;
-        if (total[4] <= price) {
-            price = total[4];
-            vector<int> v;
-            for (int j = 0; j < n; j++) {
-                if (i & (1<<j)) {
-                    v.push_back(j);
-                }
-            }
-            ret[total[4]].push_back(v);
-        }
+    for (int i = 0; i < 7; i++) {
+        if (n & (1<<i)) ret++;
     }
-    if (price == INF) {
-        cout << -1 << "\n";
-        return 0;
-    }
-    sort(ret[price].begin(), ret[price].end());
-    cout << price << "\n";
-    for (const int & i : ret[price][0]) cout << i+1 << " ";
-    cout << "\n";
+    cout << ret << "\n";
     return 0;
 }
