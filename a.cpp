00bff84958847426fc506e44d37c3296f2adab42
daehyeong2 <baconbacon1231@gmail.com>
2025-04-13 14:27:54 +0900

    (BOJ) 15353번 문제

--- a.cpp
+++ a.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, h, l, cnt, d[51][51], elv[51];
-map<int, pair<int, int>> mp;
+int c, s;
+string num1, num2, ret;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> t;
-    for (int i = 0; i < t; i++) {
-        memset(elv, 0, sizeof(elv));
-        memset(d, 0, sizeof(d));
-        mp = {}; cnt = 0;
-        cin >> h >> l;
-        for (int j = 0; j < h; j++) {
-            for (int k = 0; k < l; k++) {
-                cin >> d[j][k];
-                if (d[j][k] != -1) {
-                    mp[d[j][k]] = {j, k};
-                }
-                cnt = max(cnt, d[j][k]);
-            }
+    cin >> num1 >> num2;
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+    for (int i = 0; i < max(num1.size(), num2.size()); i++) {
+        if (i >= num1.size()) {
+            int num = num2[i] - '0' + c;
+            c = num / 10;
+            s = num % 10;
+            ret = to_string(s) + ret;
+            continue;
+        } else if (i >= num2.size()) {
+            int num = num1[i] - '0' + c;
+            c = num / 10;
+            s = num % 10;
+            ret = to_string(s) + ret;
+            continue;
         }
-        int ret = 0;
-        for (int j = 1; j <= cnt; j++) {
-            int time = mp[j].first * 20 + min(abs(elv[mp[j].first] - mp[j].second), min(l-mp[j].second + elv[mp[j].first], l-elv[mp[j].first] + mp[j].second)) * 5;
-            elv[mp[j].first] = mp[j].second;
-            ret += time;
-        }
-        cout << ret << "\n";
+        int num = (num1[i] - '0') + (num2[i] - '0') + c;
+        c = num / 10;
+        s = num % 10;
+        ret = to_string(s) + ret;
     }
+    if (c) cout << to_string(c) + ret << "\n";
+    else cout << ret << "\n";
     return 0;
 }
