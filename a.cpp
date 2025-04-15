cf704e94b8e31920932c6b88901ceeaf6279434b
daehyeong2 <baconbacon1231@gmail.com>
2025-04-15 10:34:19 +0900

    (BOJ) 15926번 문제 (풀이2)

--- a.cpp
+++ a.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt, ret, d[200004];
+int n, ret;
 string s;
 stack<int> stk;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> s;
+    stk.push(-1);
     for (int i = 0; i < n; i++) {
         if (s[i] == '(') stk.push(i);
-        else if (stk.size()) {
-            d[i] = d[stk.top()] = 1;
+        else {
             stk.pop();
+            if (stk.size()) {
+                ret = max(ret, i - stk.top());
+            } else stk.push(i);
         }
-    }
-    for (int i = 0; i < n; i++) {
-        if (d[i]) {
-            cnt++;
-            ret = max(ret, cnt);
-        } else cnt = 0;
     }
     cout << ret << "\n";
     return 0;
