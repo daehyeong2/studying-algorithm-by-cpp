286a161c774cbf91a50073813b3d9592a2b35b7f
daehyeong2 <baconbacon1231@gmail.com>
2025-04-15 20:31:39 +0900

    (BOJ) 3015번 문제 (내 힘으로 푼 2번째 플레 문제)

--- a.cpp
+++ a.cpp
@@ -1,22 +1,47 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int n, ret;
-string s;
-stack<int> stk;
+int n, a, mx;
+ll ret;
+stack<pair<int, ll>> stk;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> s;
-    stk.push(-1);
+    cin >> n;
     for (int i = 0; i < n; i++) {
-        if (s[i] == '(') stk.push(i);
-        else {
-            stk.pop();
-            if (stk.size()) {
-                ret = max(ret, i - stk.top());
-            } else stk.push(i);
+        cin >> a;
+        if (i==0) {
+            stk.push({a, 1});
+            continue;
         }
+        if (stk.top().first > a) {
+            ret++;
+            // cout << "Added " << 1 << "(" << stk.top().first << ")" << " : " << i << "\n";
+        }
+        else {
+            ret+=stk.top().second;
+            // cout << "Added " << stk.top().second << "(" << stk.top().first << ")" << " : " << i << "\n";
+        }
+        int cnt = 1;
+        while (stk.size() && a >= stk.top().first) {
+            if (a == stk.top().first) {
+                cnt+=stk.top().second;
+            }
+            mx = stk.top().first;
+            if (mx > a) continue;
+            stk.pop();
+            if (stk.size() && stk.top().first >= mx) {
+                if (stk.top().first > a) {
+                    ret++;
+                    // cout << "Added " << 1 << "(" << stk.top().first << ")" <<  " :: " << i << " " << mx << "\n";
+                } else {
+                    ret+=stk.top().second;
+                    // cout << "Added " << stk.top().second << "(" << stk.top().first << ")" <<  " :: " << i << " " << mx << "\n";
+                }
+            } else if (stk.size()) stk.pop();
+        }
+        stk.push({a, cnt});
     }
     cout << ret << "\n";
     return 0;
