6114dc75352cc88616181e05e4cae993a316d393
daehyeong2 <baconbacon1231@gmail.com>
2025-04-17 21:30:35 +0900

    5주차: 그리디 예시 문제 4

--- a.cpp
+++ a.cpp
@@ -1,47 +1,31 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-int n, a, mx;
-ll ret;
-stack<pair<int, ll>> stk;
+int n, k, m, v, c, ret;
+string s;
+vector<pair<int, int>> v1;
+vector<int> v2;
 
-int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> k;
     for (int i = 0; i < n; i++) {
-        cin >> a;
-        if (i==0) {
-            stk.push({a, 1});
-            continue;
+        cin >> m >> v;
+        v1.push_back({m, v});
+    }
+    for (int i = 0; i < k; i++) {
+        cin >> c;
+        v2.push_back(c);
+    }
+    priority_queue<int> pq;
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    int j = 0;
+    for (int i = 0; i < k; i++) {
+        while (j < n && v1[j].first <= v2[i]) pq.push(v1[j++].second);
+        if (pq.size()) {
+            ret += pq.top(); pq.pop();
         }
-        if (stk.top().first > a) {
-            ret++;
-            // cout << "Added " << 1 << "(" << stk.top().first << ")" << " : " << i << "\n";
-        }
-        else {
-            ret+=stk.top().second;
-            // cout << "Added " << stk.top().second << "(" << stk.top().first << ")" << " : " << i << "\n";
-        }
-        int cnt = 1;
-        while (stk.size() && a >= stk.top().first) {
-            if (a == stk.top().first) {
-                cnt+=stk.top().second;
-            }
-            mx = stk.top().first;
-            if (mx > a) continue;
-            stk.pop();
-            if (stk.size() && stk.top().first >= mx) {
-                if (stk.top().first > a) {
-                    ret++;
-                    // cout << "Added " << 1 << "(" << stk.top().first << ")" <<  " :: " << i << " " << mx << "\n";
-                } else {
-                    ret+=stk.top().second;
-                    // cout << "Added " << stk.top().second << "(" << stk.top().first << ")" <<  " :: " << i << " " << mx << "\n";
-                }
-            } else if (stk.size()) stk.pop();
-        }
-        stk.push({a, cnt});
     }
     cout << ret << "\n";
     return 0;
