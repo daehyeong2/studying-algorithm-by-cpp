cd849c443d07f2c646ba0ca7054c30c39310dd65
daehyeong2 <baconbacon1231@gmail.com>
2025-04-17 21:36:34 +0900

    5주차: Priority Queue 연습 문제

--- a.cpp
+++ a.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, m, v, c, ret;
-string s;
-vector<pair<int, int>> v1;
-vector<int> v2;
+int n;
+double a;
+priority_queue<double> pq;
+vector<double> ret;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> k;
+    cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> m >> v;
-        v1.push_back({m, v});
+        cin >> a;
+        pq.push(a);
+        if (pq.size() > 5) pq.pop();
     }
-    for (int i = 0; i < k; i++) {
-        cin >> c;
-        v2.push_back(c);
+    for (int i = 0; i < 5; i++) {
+        ret.push_back(pq.top()); pq.pop();
     }
-    priority_queue<int> pq;
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    int j = 0;
-    for (int i = 0; i < k; i++) {
-        while (j < n && v1[j].first <= v2[i]) pq.push(v1[j++].second);
-        if (pq.size()) {
-            ret += pq.top(); pq.pop();
-        }
-    }
-    cout << ret << "\n";
+    for (int i = 4; i >= 0; i--) cout << ret[i] << "\n";
     return 0;
 }
