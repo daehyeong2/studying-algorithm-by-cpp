a5779dcf0458dd5e3555ff832d0ad7901ff3ed04
daehyeong2 <baconbacon1231@gmail.com>
2025-04-17 23:27:55 +0900

    (BOJ) 2109번 문제

--- a.cpp
+++ a.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x, ret;
+int n, p, d, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
-    cin >> x;
-    sort(v.begin(), v.end());
-    int l = 0, r = n - 1;
-    while (l < r) {
-        if (v[l] + v[r] == x) r--, ret++;
-        else if (v[l] + v[r] > x) r--;
-        else l++;
+    for (int i = 0; i < n; i++) {
+        cin >> p >> d;
+        v.emplace_back(d, p);
     }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++) {
+        pq.push(v[i].second);
+        if (pq.size() > v[i].first) pq.pop();
+    }
+    while (pq.size()){
+        ret += pq.top();
+        pq.pop();
+    };
     cout << ret << "\n";
     return 0;
 }
