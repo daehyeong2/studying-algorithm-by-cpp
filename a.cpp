8de61a1a40ec897048bc4a8c704ef94a155f40da
daehyeong2 <baconbacon1231@gmail.com>
2025-04-17 22:07:07 +0900

    5주차: 투 포인터 예제

--- a.cpp
+++ a.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> P;
-P L[1000004];
-int n, from, to, l, r, ret;
+
+int n, x, ret;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> from >> to;
-        L[i] = {from, to};
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    cin >> x;
+    sort(v.begin(), v.end());
+    int l = 0, r = n - 1;
+    while (l < r) {
+        if (v[l] + v[r] == x) r--, ret++;
+        else if (v[l] + v[r] > x) r--;
+        else l++;
     }
-    sort(L, L + n);
-    l = L[0].first; r = L[0].second;
-    for (int i = 1; i < n; i++) {
-        if (r < L[i].first) {
-            ret += (r-l);
-            l = L[i].first; r = L[i].second;
-        } else if (L[i].first <= r && L[i].second >= r) r = L[i].second;
-    }
-    ret += r-l;
     cout << ret << "\n";
     return 0;
 }
