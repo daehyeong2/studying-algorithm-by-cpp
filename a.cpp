72ae03427b1b89c9088a91ae384a1cb27561a78b
daehyeong2 <baconbacon1231@gmail.com>
2025-06-09 23:47:45 +0900

    (BOJ) 1700번 문제

--- a.cpp
+++ a.cpp
@@ -1,26 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, p, d, ret;
-vector<pair<int, int>> v;
-priority_queue<int, vector<int>, greater<int>> pq;
+const int INF = 987654321;
+int n, k, cnt, a[104], visited[104];
+vector<int> v;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> p >> d;
-        v.emplace_back(d, p);
+int main(){
+    cin >> n >> k;
+    for (int i = 0; i < k; i++) cin >> a[i];
+    for (int i = 0; i < k; i++) {
+        if (!visited[a[i]]) {
+            if (v.size() == n) {
+                int last_pos = 0, pos;
+                for (const int a_ : v) {
+                    int here_pos = INF;
+                    for (int j = i+1; j < k; j++) {
+                        if (a_ == a[j]) {
+                            here_pos = j;
+                            break;
+                        }
+                    }
+                    if (last_pos < here_pos) {
+                        last_pos = here_pos;
+                        pos = a_;
+                    }
+                }
+                visited[pos] = 0;
+                cnt++;
+                v.erase(find(v.begin(), v.end(), pos));
+            }
+            v.push_back(a[i]); visited[a[i]] = 1;
+        }
     }
-    sort(v.begin(), v.end());
-    for (int i = 0; i < n; i++) {
-        pq.push(v[i].second);
-        if (pq.size() > v[i].first) pq.pop();
-    }
-    while (pq.size()){
-        ret += pq.top();
-        pq.pop();
-    };
-    cout << ret << "\n";
+    cout << cnt << "\n";
     return 0;
 }
