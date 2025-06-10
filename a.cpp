3dc9d0fb08e1b12b355687070941f5599d0eecde
daehyeong2 <baconbacon1231@gmail.com>
2025-06-10 20:34:10 +0900

    (BOJ) 17144번 문제

--- a.cpp
+++ a.cpp
@@ -1,37 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-int n, k, cnt, a[104], visited[104];
-vector<int> v;
+int r, c, t, ret, a[54][54], dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+
+void dfs(int y, int x, int v, bool isTop, int dir) {
+    int nextV = a[y][x];
+    a[y][x] = v;
+    int ny = y + dy[dir];
+    int nx = x + dx[dir];
+    if (ny < 0 || nx < 0 || ny >= r || nx >= c || a[ny][nx] == -1) return;
+    if (nx == c-1) {
+        if (isTop) dir = 0;
+        else dir = 2;
+    }
+    if (ny == r-1 || ny == 0) dir = 3;
+    if (nx == 0) {
+        if (isTop) dir = 2;
+        else dir = 0;
+    }
+    dfs(ny, nx, nextV, isTop, dir);
+}
 
 int main(){
-    cin >> n >> k;
-    for (int i = 0; i < k; i++) cin >> a[i];
-    for (int i = 0; i < k; i++) {
-        if (!visited[a[i]]) {
-            if (v.size() == n) {
-                int last_pos = 0, pos;
-                for (const int a_ : v) {
-                    int here_pos = INF;
-                    for (int j = i+1; j < k; j++) {
-                        if (a_ == a[j]) {
-                            here_pos = j;
-                            break;
-                        }
-                    }
-                    if (last_pos < here_pos) {
-                        last_pos = here_pos;
-                        pos = a_;
-                    }
-                }
-                visited[pos] = 0;
-                cnt++;
-                v.erase(find(v.begin(), v.end(), pos));
-            }
-            v.push_back(a[i]); visited[a[i]] = 1;
+    cin >> r >> c >> t;
+    for(int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> a[i][j];
         }
     }
-    cout << cnt << "\n";
+    while (t--) {
+        int newA[54][54];
+        memcpy(newA, a, sizeof(a));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (a[i][j] > 0) {
+                    int amount = a[i][j] / 5;
+                    int cnt = 0;
+                    for (int k = 0; k < 4; k++) {
+                        int ny = i + dy[k];
+                        int nx = j + dx[k];
+                        if (ny < 0 || ny >= r || nx < 0 || nx >= c || a[ny][nx] == -1) continue;
+                        newA[ny][nx] += amount;
+                        cnt++;
+                    }
+                    newA[i][j] -= amount * cnt;
+                }
+            }
+        }
+        memcpy(a, newA, sizeof(newA));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (a[i][j] == -1) {
+                    dfs(i, j+1, 0, a[i-1][j] != -1, 1);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (a[i][j] != -1) ret += a[i][j];
+        }
+    }
+    cout << ret << "\n";
     return 0;
 }
