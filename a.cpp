de4a18642597fb58abdc22dcea4cb1bbf8869ea9
daehyeong2 <baconbacon1231@gmail.com>
2025-08-12 19:21:13 +0900

    (BOJ) 14889번 풀이

--- a.cpp
+++ a.cpp
@@ -1,57 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, ret[5], a[1004][1004], visited[1004][1004];
-int dy[] = {-1, -1, -1, 0, 1, 1, 1, 0}, dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
-
-void dfs(int y, int x) {
-    int hap[5] = {0, 0, 0, 0, 0};
-    vector<pair<int, int>> v;
-    bool flag = false;
-    for (int i = 0; i < 8; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]) continue;
-        if (a[ny][nx] == 0) continue;
-        if (a[ny][nx] == k) {
-            flag = true;
-            continue;
-        }
-        if ((i-1) % 2 == 0) {
-            v.emplace_back(ny, nx);
-            visited[ny][nx] = 1;
-            hap[a[ny][nx]-1]++;
-        }
-    }
-    if (flag) {
-        for (int i = 0; i < 5; i++) {
-            ret[i] += hap[i];
-        }
-    } else {
-        for (int i = 0; i < v.size(); i++) {
-            visited[v[i].first][v[i].second] = 0;
-        }
-    }
-    for (int i = 1; i < 8; i+=2) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] || a[ny][nx] != 0) continue;
-        visited[ny][nx] = 1;
-        dfs(ny, nx);
-    }
-}
+int n, ret = 1e9, S[24][24];
 
 int main(){
-    cin >> n >> k;
+    cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> a[i][j];
+            cin >> S[i][j];
         }
     }
-    visited[n-1][0] = 1;
-    dfs(n-1, 0);
-    for (int i = 0; i < 5; i++) {
-        cout << ret[i] << "\n";
+    for (int num = 0; num < (1<<n); num++) {
+        int cnt = 0;
+        vector<int> players;
+        vector<int> players2;
+        for (int i = 0; i < n; i++) {
+            if (num & (1<<i)) {
+                cnt++;
+                players.push_back(i);
+            } else {
+                players2.push_back(i);
+            }
+        }
+        if (n / 2 != cnt) continue;
+        int score = 0;
+        for (int i = 0; i < cnt; i++) {
+            for (int j = 0; j < cnt; j++) {
+                score += S[players[i]][players[j]];
+            }
+        }
+        int score2 = 0;
+        for (int i = 0; i < cnt; i++) {
+            for (int j = 0; j < cnt; j++) {
+                score2 += S[players2[i]][players2[j]];
+            }
+        }
+        ret = min(ret, abs(score - score2));
+        if (ret == 0) break;
     }
+    cout << ret << "\n";
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 
-int n, x, y, w, h, adj[27][27];
-vector<vector<int>> v;
+int n, x, y, w, h, ret_cnt, adj[27];
+ll ret_s;
+vector<vector<ll>> v;
 
 int main(){
     cin >> n;
@@ -16,25 +18,37 @@ int main(){
         int i_w = v[i][2];
         int i_h = v[i][3];
         for (int j = 0; j < n; j++) {
-            if (adj[i][j] || adj[j][i]) continue;
+            if ((adj[i] & (1<<j)) || (adj[j] & (1<<i))) continue;
             int j_x = v[j][0];
             int j_y = v[j][1];
             int j_w = v[j][2];
             int j_h = v[j][3];
-            if ((i_x >= j_x && i_x <= j_x + j_w) || (i_x + i_w >= j_x && i_x + i_w <= j_x + j_w)) {
-                if ((i_y >= j_y && i_y <= j_y + j_h) || (i_y + i_h >= j_y && i_y + i_h <= j_y + j_h)) {
-                    adj[i][j] = 1;
-                    adj[j][i] = 1;
+            if ((i_x >= j_x && i_x <= j_x + j_w - 1) || (i_x + i_w - 1 >= j_x && i_x + i_w - 1 <= j_x + j_w - 1)) {
+                if ((i_y >= j_y && i_y <= j_y + j_h - 1) || (i_y + i_h - 1 >= j_y && i_y + i_h - 1 <= j_y + j_h - 1)) {
+                    adj[i] |= (1<<j);
+                    adj[j] |= (1<<i);
                 }
             }
         }
     }
     for (int i = 0; i < (1<<n); i++) {
-        for (int j = 0; j < 25; j++) {
-            if (i & (1<<j)) {
-
+        int visited = 0;
+        ll hap = 0;
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            if ((i & (1<<j)) && !(visited & adj[j])) {
+                visited |= (1<<j);
+                hap += v[j][2] * v[j][3];
+                cnt++;
             }
         }
+        if (cnt > ret_cnt) {
+            ret_cnt = cnt;
+            ret_s = hap;
+        } else if (cnt == ret_cnt && hap > ret_s) {
+            ret_s = hap;
+        }
     }
+    cout << ret_cnt << " " << ret_s << "\n";
     return 0;
 }
