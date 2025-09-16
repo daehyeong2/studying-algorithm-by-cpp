a6873faefb5f0763f073b67a58fa5d7d49a00ab5
daehyeong2 <baconbacon1231@gmail.com>
2025-09-17 08:56:31 +0900

    (BOJ) 3190번 문제 풀이

--- a.cpp
+++ a.cpp
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret = 1e9, S[24][24];
+int n, k, l, t, dir = 1, in_y, in_x, y=1, x=1, board[104][104], dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+char in_c, rotation[10004];
+queue<pair<int, int>> q;
 
 int main(){
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> S[i][j];
-        }
+    cin >> n >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> in_y >> in_x;
+        board[in_y][in_x] = 2;
     }
-    for (int num = 0; num < (1<<n); num++) {
-        int cnt = 0;
-        vector<int> players;
-        vector<int> players2;
-        for (int i = 0; i < n; i++) {
-            if (num & (1<<i)) {
-                cnt++;
-                players.push_back(i);
-            } else {
-                players2.push_back(i);
-            }
-        }
-        if (n / 2 != cnt) continue;
-        int score = 0;
-        for (int i = 0; i < cnt; i++) {
-            for (int j = 0; j < cnt; j++) {
-                score += S[players[i]][players[j]];
-            }
-        }
-        int score2 = 0;
-        for (int i = 0; i < cnt; i++) {
-            for (int j = 0; j < cnt; j++) {
-                score2 += S[players2[i]][players2[j]];
-            }
-        }
-        ret = min(ret, abs(score - score2));
-        if (ret == 0) break;
+    cin >> l;
+    for (int i = 0; i < l; i++) {
+        cin >> in_x >> in_c;
+        rotation[in_x] = in_c;
     }
-    cout << ret << "\n";
+    q.emplace(1, 1);
+    board[1][1] = 1;
+    while (++t) {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (ny > n || ny < 1 || nx > n || nx < 1 || board[ny][nx] == 1) break;
+        if (board[ny][nx] == 0) {
+            auto [fst, snd] = q.front(); q.pop();
+            board[fst][snd] = 0;
+        }
+        board[ny][nx] = 1;
+        q.emplace(ny, nx);
+        y = ny; x = nx;
+
+        if (rotation[t] == 'L') dir = (dir - 1) % 4;
+        else if (rotation[t] == 'D') dir = (dir + 1) % 4;
+        if (dir < 0) dir = 4+dir;
+    };
+    cout << t << "\n";
     return 0;
 }
