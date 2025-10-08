ecdbeb8b7782ec1a128feeb783db8d9753c5e562
daehyeong2 <baconbacon1231@gmail.com>
2025-10-09 05:57:07 +0900

    (BOJ) 1103번 문제 풀이

--- a.cpp
+++ a.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, l, t, dir = 1, in_y, in_x, y=1, x=1, board[104][104], dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
-char in_c, rotation[10004];
-queue<pair<int, int>> q;
+char ch;
+int n, m, ret, board[54][54], visited[54][54], cnt[54][54];
+
+int go(int y, int x, int k) {
+    if (cnt[y][x] && cnt[y][x] >= k) return cnt[y][x];
+    int mx = k;
+    for (int dir = 0; dir < 4; dir++) {
+        int ny = -1, nx = -1;
+        if (dir == 0) {
+            ny = y - board[y][x], nx = x;
+        } else if (dir == 1) {
+            ny = y + board[y][x], nx = x;
+        } else if (dir == 2) {
+            ny = y, nx = x - board[y][x];
+        } else if (dir == 3) {
+            ny = y, nx = x + board[y][x];
+        }
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m || board[ny][nx] == 0) continue;
+        if (visited[ny][nx]) {
+            cout << -1 << "\n";
+            exit(0);
+        }
+        visited[ny][nx] = 1;
+        int output = go(ny, nx, k+1);
+        mx = max(mx, output);
+        visited[ny][nx] = 0;
+    }
+    if (cnt[y][x] < k) cnt[y][x] = k;
+    return mx;
+}
 
 int main(){
-    cin >> n >> k;
-    for (int i = 0; i < k; i++) {
-        cin >> in_y >> in_x;
-        board[in_y][in_x] = 2;
-    }
-    cin >> l;
-    for (int i = 0; i < l; i++) {
-        cin >> in_x >> in_c;
-        rotation[in_x] = in_c;
-    }
-    q.emplace(1, 1);
-    board[1][1] = 1;
-    while (++t) {
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-        if (ny > n || ny < 1 || nx > n || nx < 1 || board[ny][nx] == 1) break;
-        if (board[ny][nx] == 0) {
-            auto [fst, snd] = q.front(); q.pop();
-            board[fst][snd] = 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> ch;
+            if (ch == 'H') board[i][j] = 0;
+            else board[i][j] = ch-'0';
         }
-        board[ny][nx] = 1;
-        q.emplace(ny, nx);
-        y = ny; x = nx;
-
-        if (rotation[t] == 'L') dir = (dir - 1) % 4;
-        else if (rotation[t] == 'D') dir = (dir + 1) % 4;
-        if (dir < 0) dir = 4+dir;
-    };
-    cout << t << "\n";
+    }
+    visited[0][0] = 1;
+    cout << go(0, 0, 1) << "\n";
     return 0;
 }
